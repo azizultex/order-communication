9814b1ee991a2bed3cac3f21572eede42e9c1b0b
Saiful Islam <saifulislam265@gmail.com>
2022-02-16 19:27:41 +0600

    added notification read functionality and fix us issue

--- hub-order-communication.php
+++ hub-order-communication.php
@@ -99,13 +99,34 @@ final class OderCommunication {
             </div>
         </form>
         <?php
+        if(isset($_POST['create_reply'])){
+            $comments = isset($_POST['agent_replies']) ? $_POST['agent_replies'] : '';
+            $comment_id = wp_insert_post(
+                array(
+                    'post_title'=> intval($attributes['order_id']),
+                    'post_type'=>'order_communication',
+                    'post_content'=> $comments,
+                    'post_status'  => 'publish',
+                    )
+            );
+            $users_wp = preg_match_all('/data-item-id="(.*?)"/', stripslashes($comments), $matches);
+            add_post_meta($comment_id, 'order_id', intval($attributes['order_id']));
+            add_post_meta($comment_id, 'mentioned_user', $matches[1]);
+            add_post_meta($comment_id, 'agent_replied', get_current_user_id());
+            add_post_meta($comment_id, 'post_id', intval($attributes['post_id']));
+            $author_obj = get_user_by('login', $matches[1][0]);
+            $notification_count = get_user_meta($author_obj->data->ID, 'notification_count');
+            $notification_count[0] += 1;
+            update_user_meta($author_obj->data->ID, 'notification_count', $notification_count[0]);
+        }
+        
         $args = array(
             'posts_per_page' => -1,
             'post_type' => 'order_communication',
             'meta_query' => array(
                 array(
                     'key'     => 'order_id',
-                    'value'   => $attributes['order_id'],
+                    'value'   => intval($attributes['order_id']),
                     'compare' => 'LIKE',
                 ),
             ),
@@ -117,9 +138,20 @@ final class OderCommunication {
                 $agent_replied = get_post_meta(get_the_ID(), 'agent_replied', true);
                 ?>
                 <div class="coversation_timeline">
-                    <div class="conversation_single">
-                        <img src="<?php echo get_avatar_url($agent_replied) ?>" >
-                        <?php the_content(); ?>
+                    <div class="replied_by">
+                        <img src="<?php echo get_avatar_url($agent_replied); ?>" >
+                        <div class="agent_reply_time">
+                            <?php
+                            $agent_replied = get_post_meta(get_the_ID(), 'agent_replied', true);
+                            $user_obj = get_user_by('ID', $agent_replied);
+                            echo '<span class="agent_name">'.$user_obj->data->display_name.'</span>';
+                            echo '<span class="comment_date"><i>'.get_the_date('Y-m-d h:i:sa').'</i></span>';
+                            ?>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="replied_content">
+                        <?php echo get_the_content(); ?>
                     </div>
                 </div>
                 <hr>
@@ -129,25 +161,6 @@ final class OderCommunication {
                 $notification_count = !empty(get_user_meta($author_obj->data->ID, 'notification_count'))? get_user_meta($author_obj->data->ID, 'notification_count'): 0;
                 //write_log($author_obj->data->ID);
             }
-        }
-        if(isset($_POST['create_reply'])){
-            $comments = $_POST['agent_replies'];
-            $comment_id = wp_insert_post(
-                array(
-                    'post_title'=>$attributes['order_id'],
-                    'post_type'=>'order_communication',
-                    'post_content'=> $comments,
-                    'post_status'  => 'publish',
-                    )
-            );
-            $users_wp = preg_match_all('/data-item-id="(.*?)"/', stripslashes($comments), $matches);
-            add_post_meta($comment_id, 'order_id', $attributes['order_id']);
-            add_post_meta($comment_id, 'mentioned_user', $matches[1]);
-            add_post_meta($comment_id, 'agent_replied', get_current_user_id());
-            add_post_meta($comment_id, 'post_id', $attributes['post_id']);
-            $author_obj = get_user_by('login', $matches[1][0]);
-            $notification_count = !empty(get_user_meta($author_obj->data->ID, 'notification_count')) || get_user_meta($author_obj->data->ID, 'notification_count') == 0 ? get_user_meta($author_obj->data->ID, 'notification_count'): 1;
-            update_user_meta($author_obj->data->ID, 'notification_count', $notification_count);
         }
         return ob_get_clean();
     }
